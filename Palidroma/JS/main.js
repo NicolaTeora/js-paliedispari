0b88d0bf915da5ef71872402585d71695929ae2d
NicolaTeora <nikxodia@gmail.com>
2024-01-17 18:52:52 +0100

    feat : word lecter in array

--- Palidroma/JS/main.js
+++ Palidroma/JS/main.js
@@ -1,18 +1,35 @@
 const wordUser = document.getElementById('wordInput');
 const btnControl = document.getElementById('controlInput');
+let wordOriginal = [];
+let wordPalindroma = [];
 
 // Creo un input per chiedere la parola all'utente
 btnControl.addEventListener('click', function(){
     let wordControlled = wordUser.value;
     console.log(wordControlled);
-    // console.log(wordControlled[])
+    //console.log(wordControlled.length);
+
+    let b = 0;
+    // 1 crea un array con le lettere della parola
+    for (let i = 0; i < wordControlled.length; i++) {
+        b++; 
+        wordOriginal.push(wordControlled[i]);
+    }
+    console.log(wordOriginal);
+    console.log(b);
+
+    
 });
 
-// creo la funzione per verificare che la parola inserita sia palindroma
-// function reverseWord(){
+    
 
-//     for (let i = 0; i <= ?? ; i++){
-        
-//     }
-   
-// }
+//function isPalindroma(word){}
+
+//for (let i = 0; i < array.length; i++) {
+//    const element = array[i];
+//}
+
+// 2 in un secondo array mettici le lettere al crotrario
+//for (let i = 0; i < array.length; i++) {
+//    const element = array[i];
+// 3 unisci il secondo array
