af2c5a5d6e8052cfb68ba55122f6f34c0ee3a909
NicolaTeora <nikxodia@gmail.com>
2024-01-18 16:08:42 +0100

    feat : number user

--- Pari e Dispari/JS/main.js
+++ Pari e Dispari/JS/main.js
@@ -1,15 +1,18 @@
-const userNumber = parseInt(document.getElementById('user-number'));
+const userNumber = document.getElementById('user-number');
 let computerNumber = crateNumberPc();
 const goPlay = document.getElementById('play-game');
 
+//tramite una funzione produco un numero random per il computer
 function crateNumberPc(){
     let numerRandom = Math.floor(Math.random() * 5) + 1;
     return numerRandom;
 }
 
+// Chiedo all'utente tramite un input di inserire un numero
 goPlay.addEventListener('click', function(){
+    let numberSelect = parseInt(userNumber.value);
+    console.log(numberSelect)
     
-} )
+})
 
-console.log(userNumber)
 //console.log()
